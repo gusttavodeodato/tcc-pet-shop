80d2bf56a1c393d43181c6d17dccb5f749fba106
gusttavodeodato <guustavodeodato@outlook.com>
2026-04-07 13:03:11 -0300

    first commit

---
+++ PetShopTCC/src/main/java/dev/deodato/tcc/petshop/PetShopTccApplication.java
@@ -0,0 +1,13 @@
+package dev.deodato.tcc.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopTccApplication.class, args);
+	}
+
+}
---
+++ PetShopTCC/src/main/java/dev/deodato/tcc/petshop/controller/CadastroController.java
@@ -0,0 +1,15 @@
+package dev.deodato.tcc.petshop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CadastroController {
+
+    @GetMapping("/cadastro")
+    public String boasVindas() {
+        return "Primeira mensagem nessa rota";
+    }
+}
